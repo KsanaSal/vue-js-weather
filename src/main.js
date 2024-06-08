81c2174225a5cf22fdb6ff2a084f62919b4dcf47
KsanaSal <oasalivon@gmail.com>
2024-06-08 19:19:56 +0300

    changed main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { router } from '@/router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
+
+// createApp(App).mount('#app')
